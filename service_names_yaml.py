fadf9d33b067c5df8319bc880838b8fb60fca0ef
Aleksey Agafonov <a_agafonov@lesta.group>
2022-08-10 16:51:43 +0300

    add service names yaml script

---
+++ service_names_yaml.py
@@ -0,0 +1,30 @@
+import yaml
+import xlsxwriter
+
+
+# Script takes yaml file, parse it and store name of services (1st layer) to Excel file
+
+yamlpath = '~/test.yaml'
+realm = 'test'  # yaml name (e.g. yaml for specific environment)
+
+row = 0
+column = 0
+workbook_name = 'services_{}.xlsx'.format(realm)
+
+with open(yamlpath) as f:
+    result =  yaml.load(f, Loader=yaml.Loader)
+yaml_entities = list(result.keys())
+
+with xlsxwriter.Workbook(workbook_name) as workbook:
+    worksheet = workbook.add_worksheet()
+
+    for data in yaml_entities:
+        worksheet.write(row, column, data)
+        row += 1
+
+if __name__ == "__main__":
+  print('All services are saved in {}'.format(workbook_name))
+  print('')
+  print(yaml_entities)
+  print('')
+  print('Amount of yaml entities for yaml={}:'.format(yamlpath), len(yaml_entities))
